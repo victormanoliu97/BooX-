3ecedb43c7dfa859861d82a8aff32e6632dcdf49
pandaismyname1@gmail.com <TRUCRINeO44>
2018-03-08 10:36:34 +0200

    added a new "submit book offer" template, also made a python script to extract json content V2

---
+++ extractor.py
@@ -0,0 +1,11 @@
+import json, re
+text = ""
+with open("genres.js",'r') as file:
+    text = file.read()
+matches = re.findall(r'(.*) \xe2(.*)\n',text)
+jsonMatches = []
+for match in matches:
+   jsonMatches.append(match[0])
+print jsonMatches
+with open("dump.json",'w') as file:
+    file.write(json.dumps(jsonMatches))
---
+++ genres.js
@@ -0,0 +1,35 @@
+Classic – fiction that has become part of an accepted literary canon, widely taught in schools
+Comics/Graphic novel – comic magazine or book based on a sequence of pictures (often hand drawn) and few words.
+Crime/detective – fiction about a crime, how the criminal gets caught, and the repercussions of the crime
+Fable – legendary, supernatural tale demonstrating a useful truth
+Fairy tale – story about fairies or other magical creatures
+Fan fiction – fiction written by a fan of, and featuring characters from, a particular TV series, movie, or book
+Fantasy – fiction with subplot(s), theme(s), major and minor characters, in which the narrative is presented in verse form (usually free verse)
+Folklore – the songs, stories, myths, and proverbs of a people or "folk" as handed down by word of mouth
+Historical fiction – story with fictional characters and events in an historical setting
+Horror – fiction in which events evoke a feeling of dread and sometimes fear in both the characters and the reader
+Humor – usually a fiction full of fun, fancy, and excitement, meant to entertain and sometimes cause intended laughter; but can be contained in all genres
+Legend – story, sometimes of a national or folk hero, that has a basis in fact but also includes imaginative material
+Magical realism – story where magical or unreal elements play a natural part in an otherwise realistic environment
+Mystery – fiction dealing with the solution of a crime or the revealing of secrets
+Mythology – legend or traditional narrative, often based in part on historical events, that reveals human behavior and natural phenomena by its symbolism; often pertaining to the actions of the gods
+Mythopoeia – fiction in which characters from religious mythology, traditional myths, folklore and/or history are recast into a re-imagined realm created by the author
+Picture book – picture storybook is a book with very little words and a lot of pictures; picture stories are usually for children
+Realistic fiction – story that is true to life
+Science fiction – story based on the impact of actual, imagined, or potential science, often set in the future or on other planets
+Short story – fiction of great brevity, usually supports no subplots
+Suspense/thriller – fiction about harm about to befall a person or group and the attempts made to evade the harm
+Tall tale – humorous story with blatant exaggerations, such as swaggering heroes who do the impossible with nonchalance
+Western  – fiction set in the American Old West frontier and typically in the late eighteenth to late nineteenth century
+Common genres: non-fiction
+Biography  – narrative of a person's life; when the author is also the subject, this is an autobiography.
+Essay  – a short literary composition that reflects the author's outlook or point.
+Owner's manual (also Instruction manual, User's guide)  – an instructional book or booklet that is supplied with consumer products such as vehicles, home appliances, firearms, toys and computer peripherals
+Journalism – reporting on news and current events
+Lab Report  – a report of an experiment
+Memoir  – factual story that focuses on a significant relationship between the writer and a person, place, or object; reads like a short novel
+Narrative nonfiction/personal narrative  – factual information about a significant event presented in a format that tells a story
+Reference book  – such as a dictionary, thesaurus, encyclopedia, almanac, or atlas
+Self-help book  – information with the intention of instructing readers on solving personal problems.
+Speech  – public address or discourse
+Textbook  – authoritative and detailed factual description of a topic.
