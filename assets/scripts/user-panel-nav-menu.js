c3ea726925ee07fcad13625a93a7562716611359
Victor Manoliu <victormanoliu97@gmail.com>
2018-03-09 12:29:03 -0800

    Basic searchbar, stylled navbar and linked the add book template

--- assets/scripts/user-panel-nav-menu.js
+++ assets/scripts/user-panel-nav-menu.js
@@ -1,7 +1,16 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "150px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+}
+
+function createPopup(url) {
+    var newWindow = window.open(url, 'bookOffer', 'height=620, width=900');
+
+    if (window.focus) {
+        newWindow.focus();
+    }
+    return false;
 }
