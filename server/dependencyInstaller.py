83d4e653e05b9eb5ced61a9f1b1fa4215f0d805b
Paius Emanuel Andrei <pandaismyname1@gmail.com>
2018-03-15 18:59:18 +0200

    Added a python package automatic installer for backend dependencies

--- server/dependencyInstaller.py
+++ server/dependencyInstaller.py
@@ -3,3 +3,11 @@ try:
     import cx_Oracle
 except:
     Popen(['py','-m','pip','install','cx_Oracle'])
+try:
+    import json
+except:
+    Popen(['py','-m','pip','install','json'])
+try:
+    import console_menu
+except:
+    Popen(['py','-m','pip','install','console-menu'])
