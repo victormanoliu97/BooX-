9f553828c28274d94054ccf84d5bcacd6452ef87
Paius Emanuel Andrei <pandaismyname1@gmail.com>
2018-03-21 16:02:04 +0200

    Added filters for the search

--- assets/scripts/user-panel-nav-menu.js
+++ assets/scripts/user-panel-nav-menu.js
@@ -1,11 +1,49 @@
 var navOpen = false;
+var filterOpen = false;
 var bookEntryLayout;
 var sidePanelHTML;
+var filterContainerHTML;
+
 function openNav(steps) {
     if (steps>=0)
     {
         document.body.style.setProperty("grid-template-columns",((15-steps)/4).toString()+"em auto");
         setTimeout(function(){openNav(steps-1)},1000/60);
+    }
+}
+function openFilters(steps) {
+    if (steps>5)
+    {
+        document.getElementById("filterContainer").style.setProperty("height",(3+(10-steps)).toString()+"em");
+        setTimeout(function(){openFilters(steps-1)},1000/60);
+    }
+    else if (steps==5)
+    {
+        document.getElementById("filterContainer").innerHTML = filterContainerHTML;
+        document.getElementById("filterContainer").style.opacity = 0;
+        setTimeout(function(){openFilters(steps-1)},1000/60);
+    }
+    else if (steps>=0)
+    {
+        document.getElementById("filterContainer").style.opacity = 1-(0.2*steps);
+        setTimeout(function(){openFilters(steps-1)},1000/60);
+    }
+}
+function closeFilters(steps) {
+    if (steps>5)
+    {
+        document.getElementById("filterContainer").style.opacity = (1-(0.1*(15-steps)));
+        setTimeout(function(){closeFilters(steps-1)},1000/60);
+    }
+    else if (steps==5)
+    {
+        document.getElementById("filterContainer").innerHTML = "";
+        setTimeout(function(){closeFilters(steps-1)},1000/60);
+    }
+    else if (steps>=0)
+    {
+        document.getElementById("filterContainer").style.setProperty("height",(1+steps).toString()+"em");
+        setTimeout(function(){closeFilters(steps-1)},1000/60);
     }
 }
 
@@ -21,12 +59,23 @@ function closeNav(steps)
         document.getElementById("sidePanel").innerHTML = "";
     }
 }
+function switchFilters()
+{
+    if(filterOpen)
+    {
+        closeFilters(15);
+        filterOpen = false;
+    }
+    else
+    {
+        openFilters(15);
+        filterOpen = true;
+    }
+}
 function switchNav()
 {
     if(navOpen)
     {
-        document.getElementById("sidePanelButton").classList.remove("fa-arrow-circle-left");
-        document.getElementById("sidePanelButton").classList.add("fa-arrow-circle-right");
         closeNav(15);
         navOpen = false;
     }
@@ -49,6 +98,11 @@ function createPopup(url) {
     return false;
 }
 
+function updateDistanceFilter()
+{
+    document.getElementById("distanceLabel").innerText = "Max distance: " + document.getElementById("distance").value
+}
+
 
 function init()
 {
@@ -56,6 +110,8 @@ function init()
     document.getElementById("sidePanel").innerHTML = "";
     bookEntryLayout = document.getElementById("main").innerHTML;
     document.getElementById("main").innerHTML = "";
+    filterContainerHTML = document.getElementById("filterContainer").innerHTML;
+    document.getElementById("filterContainer").innerHTML = "";
     console.log("a");
     for (var i=0;i<30;i++)
     {
