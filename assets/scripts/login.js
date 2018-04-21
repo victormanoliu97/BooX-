2dab6d07af7f4149198f7932ca69d24bfdf22692
Paius Emanuel Andrei <pandaismyname1@gmail.com>
2018-04-21 20:55:10 +0300

    Repaired sign out

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -29,6 +29,7 @@ function onLoadGoogleAuthApi() {
     
   }
 function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
       window.location.replace("index.html");
