d8abd743bbd505c26507006d022fb642eea44b37
Victor Manoliu <victormanoliu97@gmail.com>
2018-05-09 01:58:33 -0700

    Prettyfied html report generator using ploty

--- server/exporters/exportManagerHTML.py
+++ server/exporters/exportManagerHTML.py
@@ -1,6 +1,8 @@
 import cx_Oracle
 import os 
 from io import StringIO
+import plotly as py
+import plotly.graph_objs as go 
 
 conn = cx_Oracle.connect('TW/TWBooX@localhost:1521')
 cursor = conn.cursor()
@@ -10,30 +12,39 @@ from exportManager import getNumberOfGenres
 from exportManager import getNumberOfLanguages
 from exportManager import userReport
 
+data = [go.Bar(
+    x = ['Number of genres', 'Number of languages', 'Number of offers'],
+    y = [userReport[0], userReport[1], userReport[2]]
+)]
 
-html_string = """
-<!DOCTYPE html>
-<html>
-    <head>
-    <meta charset="UTF-8">
-    <title>HTML-REPORT</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>body{ margin:0 100; background:whitesmoke; }</style>
-    </head>
-    <body>
-        <h1>Reports so far for Offers, Languages and Genres </h1>
-        <ul>
-        <li>Number of genres:""" + userReport[0] + """
-        <li>Number of languages:""" + userReport[1] + """
-        <li>Number of offers:""" + userReport[2] + """
-        <li>Genres existing:""" + str(userReport[3]) + """
-        <ul>
-    </body>
-</html>"""
+py.offline.plot(
+    data, filename='../../assets/reports/report.html'    
+)
 
-f = open("../../assets/reports/report.html", 'w')
-f.write(html_string)
-f.close()
 
-import webbrowser
-webbrowser.open("report.html")
+# html_string = """
+# <!DOCTYPE html>
+# <html>
+#     <head>
+#     <meta charset="UTF-8">
+#     <title>HTML-REPORT</title>
+#     <meta name="viewport" content="width=device-width, initial-scale=1">
+#     <style>body{ margin:0 100; background:whitesmoke; }</style>
+#     </head>
+#     <body>
+#         <h1>Reports so far for Offers, Languages and Genres </h1>
+#         <ul>
+#         <li>Number of genres:""" + userReport[0] + """
+#         <li>Number of languages:""" + userReport[1] + """
+#         <li>Number of offers:""" + userReport[2] + """
+#         <li>Genres existing:""" + str(userReport[3]) + """
+#         <ul>
+#     </body>
+# </html>"""
+
+# f = open("../../assets/reports/report.html", 'w')
+# f.write(html_string)
+# f.close()
+
+# import webbrowser
+# webbrowser.open("report.html")
