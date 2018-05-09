4c34700abd1092311d5ebfcede5c78f753bed559
Victor Manoliu <victormanoliu97@gmail.com>
2018-05-09 02:07:16 -0700

    Refractored html report generator : removed comments

--- server/exporters/exportManagerHTML.py
+++ server/exporters/exportManagerHTML.py
@@ -20,31 +20,3 @@ data = [go.Bar(
 py.offline.plot(
     data, filename='../../assets/reports/report.html'    
 )
-
-
-# html_string = """
-# <!DOCTYPE html>
-# <html>
-#     <head>
-#     <meta charset="UTF-8">
-#     <title>HTML-REPORT</title>
-#     <meta name="viewport" content="width=device-width, initial-scale=1">
-#     <style>body{ margin:0 100; background:whitesmoke; }</style>
-#     </head>
-#     <body>
-#         <h1>Reports so far for Offers, Languages and Genres </h1>
-#         <ul>
-#         <li>Number of genres:""" + userReport[0] + """
-#         <li>Number of languages:""" + userReport[1] + """
-#         <li>Number of offers:""" + userReport[2] + """
-#         <li>Genres existing:""" + str(userReport[3]) + """
-#         <ul>
-#     </body>
-# </html>"""
-
-# f = open("../../assets/reports/report.html", 'w')
-# f.write(html_string)
-# f.close()
-
-# import webbrowser
-# webbrowser.open("report.html")
