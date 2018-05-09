408f9a761ec6b4a6f7953ed59bc6550862c12a44
Paius Emanuel Andrei <pandaismyname1@gmail.com>
2018-05-09 11:05:48 +0300

    Added failsafes for exporter builder

--- server/exporters/Buildăr.py
+++ server/exporters/Buildăr.py
@@ -1,13 +1,17 @@
 from subprocess import Popen
 from time import sleep
+import os
 
 running = True
 
 while running:
     sleep(30)
-    htmlReportProcess = Popen(['python','exportManagerHTML.py'])
-    htmlReportProcess = Popen(['python','exportManagerXML.py'])
-    htmlReportProcess = Popen(['python','exportManagerJSON.py'])
+    if os.path.isfile(os.path.join(os.path.dirname(__file__),'exportManagerHTML.py')):
+        htmlReportProcess = Popen(['python','exportManagerHTML.py'])
+    if os.path.isfile(os.path.join(os.path.dirname(__file__),'exportManagerXML.py')):
+        htmlReportProcess = Popen(['python','exportManagerXML.py'])
+    if os.path.isfile(os.path.join(os.path.dirname(__file__),'exportManagerJSON.py')):
+        htmlReportProcess = Popen(['python','exportManagerJSON.py'])
 
 def close():
     running = False
