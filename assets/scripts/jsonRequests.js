4639b27d844577697eb669cb18c86387446d11f4
Paius Emanuel Andrei <pandaismyname1@gmail.com>
2018-05-28 10:49:04 +0300

    added a jsonRequestHandelr for easy requests to the server

---
+++ assets/scripts/jsonRequests.js
@@ -0,0 +1,14 @@
+function post(json,destination,callback)
+{
+    var jsonField = "json=" + JSON.stringify(json);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", destination);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200)
+        {
+            callback(this);
+        }
+    };
+    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttp.send(jsonField);
+}
